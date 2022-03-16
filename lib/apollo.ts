fe2462a422f521b10908508fa29630b80d576198
MortonaPreguica <raul31fish@gmail.com>
2022-03-16 15:30:05 -0300

    Updating Apolli

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { relayStylePagination } from '@apollo/client/utilities';
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:3000/api/graphql',
+  uri: 'https://only-this-time.vercel.app/api/graphql',
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
